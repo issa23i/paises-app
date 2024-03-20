8017d6a75cf19c1b0101a545eac1a9bb9289356a
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-20 11:07:40 +0100

    RouterModule, RouterLink, RouterLinkActive, ejercicio contactPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
+
+
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+
+const routes: Routes = [
+
+  {
+    path: 'about',
+    pathMatch: 'full',
+    component: AboutPageComponent
+  },
+  {
+    path:'contact',
+    pathMatch: 'full',
+    component: ContactPageComponent
+  },
+  {
+    path:'home',
+    pathMatch: 'full',
+    component: HomePageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
