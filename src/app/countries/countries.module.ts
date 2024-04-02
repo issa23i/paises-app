f8bb5d5170f4817e761d9dfb53f5e6fb5834ad5d
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-04-02 08:37:50 +0200

    table

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -10,6 +10,7 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
 
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -18,7 +19,8 @@ import { SharedModule } from '../shared/shared.module';
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
