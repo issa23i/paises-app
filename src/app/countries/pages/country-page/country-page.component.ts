8d931c202bcabcb773dc8b862edf709f0b4b566a
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-04-02 12:43:27 +0200

    switchMap

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/countries';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,9 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
+
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -26,8 +30,8 @@ export class CountryPageComponent implements OnInit {
       if(!country){
         return this.router.navigateByUrl('')
       }
-      console.log('Tenemos un país')
-      return;
+      return this.country = country
+
       });
   }
 }
